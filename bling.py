198b0c2ae0775617a965da291709f2da0f728e69
elliotnunn <elliotnunn@me.com>
2014-01-19 14:18:54 +0000

    menus waaaay nicer, etc

--- bling.py
+++ bling.py
@@ -2,6 +2,7 @@
 
 import threading
 import pygame
+import pygame.freetype
 import os
 import time
 import random
@@ -147,8 +148,8 @@ class PixelChucker(Server):
         # Based on "Python GUI in Linux frame buffer"
         # http://www.karoltomala.com/blog/?p=679
         disp_no = os.getenv("DISPLAY")
-        if disp_no:
-            print "I'm running under X display = {0}".format(disp_no)
+        # if disp_no:
+        #     print "I'm running under X display = {0}".format(disp_no)
         
         # Check which frame buffer drivers are available
         # Start with fbcon since directfb hangs with composite output
@@ -161,7 +162,7 @@ class PixelChucker(Server):
             try:
                 pygame.display.init()
             except pygame.error:
-                print 'Driver: {0} failed.'.format(driver)
+                #print 'Driver: {0} failed.'.format(driver)
                 continue
             found = True
             break
@@ -170,7 +171,7 @@ class PixelChucker(Server):
             raise Exception('No suitable video driver found!')
         
         size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-        print "Framebuffer size: %d x %d" % (size[0], size[1])
+        #print "Framebuffer size: %d x %d" % (size[0], size[1])
         self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
         # Clear the screen to start
         self.screen.fill((127, 0, 127))        
@@ -195,52 +196,63 @@ class PixelChucker(Server):
 
 
 class Compositor(Client, Server):
-    def prepare_to_draw_frame(self):
-        "Nothing"
+    def pre_frame(self): "Nothing"
     
     def draw_frame(self, buffer):
+        self.client_list_lock.acquire()
         
-        my_back_buffer = buffer
-        
-        buffer.fill((0, 0, 0))
-        
+        self.pre_frame() # this will update the x and y coordinates in self.clients
+                
         # one day this will be smarter:
         # - only drawing the topmost fullscreen window and above
         # - drawing a transformed image at the coordinates set by an animation
         # my_back_buffer.fill((255, 255, 255))
-        self.client_list_lock.acquire()
-        offset = 0
-        for client in self.clients:
+        
+        first_to_draw = 0
+        for i in reversed(range(0, len(self.clients))):
+            client_tuple = self.clients[i]
+            if client_tuple[1:3] == (0, 0):
+                if client_tuple[0].width == self.width and client_tuple[0].height == self.height:
+                    first_to_draw = i
+                    break
+        if first_to_draw < len(self.clients) - 2: print("inefficient!")
+        
+        for i in range(first_to_draw, len(self.clients)):
+            client_tuple = self.clients[i]
+            client = client_tuple[0]
+            offset = client_tuple[1:3]
+            
             # Client may not swap buffers while we are blitting its front buffer!
             client.buffer_lock.acquire()
-            client_front_buffer = client.get_buffer(True)
-            my_back_buffer.blit(client_front_buffer, (offset, offset))
+            buffer.blit(client.get_buffer(True), offset)
             client.buffer_lock.release()
-            offset += 0
-        
+
         self.client_list_lock.release()
         
     def add_client(self, client):
         self.client_list_lock.acquire()
-        self.clients.append(client)
+        self.clients.append((client, 0, 0))
         self.client_list_lock.release()
+        
         client.parent_server = self
         self.dirty.set()
     
-    def remove_client(self, client): # this is spaghetti
+    def remove_client(self, client):
         self.client_list_lock.acquire()
-        if client in self.clients: self.clients.remove(client)
+        for i in reversed(range(0, len(self.clients))):
+            if self.clients[i][0] == client:
+                self.clients.pop(i)
+                break
         self.client_list_lock.release()
-        #client.parent_server = None
-        client.event("offscreen")
+        
+        client.parent_server = None
         self.dirty.set()
+        client.event("offscreen")
     
     def notify_client_dirty(self):
         self.dirty.set()
     
     def __init__(self, graf_props):
-        # init the super
-        
         Client.__init__(self, graf_props)
         Server.__init__(self)
         
@@ -253,17 +265,69 @@ class Compositor(Client, Server):
             self.dirty.set()
             
             self.client_list_lock.acquire()
-            for client in self.clients:
-                client.event("quit")
+            for client_tuple in self.clients:
+                client_tuple[0].event("quit")
             self.client_list_lock.release()
         else:
             self.client_list_lock.acquire()
             if len(self.clients) > 0:
-                client = self.clients[-1]
+                client_tuple = self.clients[-1]
                 self.client_list_lock.release()
-                client.event(event)
+                client_tuple[0].event(event)
             else:
                 self.client_list_lock.release()
+        
+
+
+class FabCompositor(Compositor):
+    def pre_frame(self):
+        for i in range(0, len(self.clients)):
+            # The tuples in self.clients are of the form (client, x_pos, y_pos) by default.
+            # This subclass extends them to (client, x_pos, y_pos, animation),
+            # where animation = (start_time_in_s, duration_in_s, direction), and
+            # direction is -1 for sliding out to the right, and 1 for sliding in from the right
+            (client, x, y, animation) = self.clients[i][0:4]
+            
+            if animation != None:
+                (start_time, duration, direction) = animation #unpack the tuple in the tuple in self.clients
+                progress = (time.clock() - start_time) / duration * self.width
+                
+                if progress >= self.width: #put a stop to this animation
+                    animation = None
+                    if direction == -1: # this was a fuck-off animation
+                        self.clients.pop(i) # DANGEROUS
+                        client.parent_server = None
+                        client.event("offscreen")
+                    else: # the client remains visible once the animation ends
+                        self.clients[i] = (client, 0, 0, None)
+                        
+                else: 
+                    if direction == -1: # ie sliding out to the right
+                        x = progress
+                    elif direction == 1: # sliding in from the right
+                        x = self.width - progress
+                        
+                    self.clients[i] = (client, x, 0, animation)
+            
+                self.dirty.set() # go like mad
+    
+    def add_client(self, client):
+        self.client_list_lock.acquire()
+        self.clients.append((client, 0, 0, (time.clock(), 0.2, 1)))
+        self.client_list_lock.release()
+        
+        client.parent_server = self
+        self.dirty.set()
+    
+    def remove_client(self, client):
+        self.client_list_lock.acquire()
+        for i in reversed(range(0, len(self.clients))):
+            if self.clients[i][0] == client:
+                self.clients[i] = (self.clients[i][0], 0, 0, (time.clock(), 0.2, -1))
+                break
+        self.client_list_lock.release()
+        
+        self.dirty.set()
         
 
 class Clock(Client):
@@ -280,7 +344,6 @@ class Clock(Client):
         
         self.count = 0
         self.font = pygame.font.SysFont("chicagoflf", 12)
-        print str(self.font.get_linesize())
         
         self.bg = (0, 0, 0); self.fg = (255, 255, 255);
         
@@ -294,54 +357,77 @@ class Clock(Client):
 
 class ProtoMenu(Client):
     def draw_frame(self, buffer):
-        # draw a frame!
+        blk = (0, 0, 0)
+        wht = (255, 255, 255)
+        
+        # draw the title
+        pygame.draw.rect(buffer, wht, (0, 0, self.width, self.titlearea_height))
+        title_width = self.font.get_rect(self.title)[2]
+        self.font.render_to(buffer, ((self.width - title_width) // 2, 0), self.title, fgcolor=blk, bgcolor=wht)
+        
+        # draw a separating line after 10 pixels
+        pygame.draw.line(buffer, blk, (0, self.titlearea_height - 2), (self.width, self.titlearea_height - 2))
+        # equals 12 pixels, leaving room for 4 lines of chicago
+        
+        # draw the "contents"
         draw_first = self.scroll // self.item_height
         draw_last = (self.scroll + self.view_height - 1) // self.item_height + 1
-    
+        
         for item_index in range(draw_first, draw_last):
             if item_index == self.selected:
-                fg = (255, 255, 255); bg = (0, 0, 0);
+                tc, bc = wht, blk
             else:
-                bg = (255, 255, 255); fg = (0, 0, 0);
+                tc, bc = blk, wht
         
-            y = item_index * self.item_height - self.scroll
+            y = self.titlearea_height + item_index * self.item_height - self.scroll
         
-            pygame.draw.rect(buffer, bg, (0, y, self.view_width, self.item_height))
+            pygame.draw.rect(buffer, bc, (0, y, self.view_width, self.item_height))
             
             if item_index < self.item_count:
-                text = self.font.render(self.items[item_index][0], False, fg, bg)
-                buffer.blit(text, (4, y))
+                text = self.font.render(self.items[item_index][0], fgcolor=tc, bgcolor=bc)
+                buffer.blit(text[0], (4, y+1))
         
-        bg = (255, 255, 255); fg = (0, 0, 0);
-        pygame.draw.line(buffer, fg, (self.view_width, 0), (self.view_width, self.view_height))
-        pygame.draw.rect(buffer, bg, (self.view_width+1, 0, 9, self.view_height))
-        sb_y = self.scroll * self.view_height / self.content_height
-        sb_h = self.view_height * self.view_height / self.content_height
-        pygame.draw.rect(buffer, fg, (self.view_width+1, sb_y, 9, sb_h))
+        if self.scrollbar_visible:
+            pygame.draw.rect(buffer, wht,(self.view_width, self.titlearea_height, self.scrollbararea_width, self.view_height))
+            pygame.draw.line(buffer, blk, (self.view_width + 1, self.titlearea_height - 1), (self.view_width + 1, self.height))
+            pygame.draw.rect(buffer, blk, (self.view_width + 3, self.scrollbar_pos + self.titlearea_height, self.scrollbararea_width - 3, self.scrollbar_height))
     
-    def __init__(self, graf_props, items):
+    def __init__(self, graf_props, items, title="untitled"):
         Client.__init__(self, graf_props)
+        
+        # will NOT change
+        self.title = title.upper()
         
         self.items = items # list of tuples: ("Artists>>>", a function)
         self.item_count = len(self.items)
         
-        self.view_width, self.view_height = self.width, self.height
-        self.font = pygame.font.SysFont("ChicagoFLF", 12)
-        self.item_height = self.font.get_linesize()
+        self.titlearea_height = 12
+        self.view_width, self.view_height = self.width, self.height - self.titlearea_height
+        
+        self.font = pygame.freetype.Font("chicago.bdf")
+        self.item_height = self.font.get_sized_glyph_height()
+        
         self.content_height = self.item_height * self.item_count
-        self.show_scrollbar = self.content_height > self.view_height
+        if self.content_height <= self.view_height:
+            self.scrollbar_visible = False
+            self.scrollbararea_width = 0
+        else:
+            self.scrollbar_visible = True
+            self.scrollbararea_width = 12
+            self.scrollbar_height = max(10, self.view_height * self.view_height / self.content_height)
         
-        if self.show_scrollbar:
-            self.view_width -= 10
-        
+        self.view_width = self.width - self.scrollbararea_width
+                
         # will change:
         self.scroll = 0
         self.selected = 0
+        self.scrollbar_pos = 0
         
         self.dirty.set()
         
     def event(self, event):
         do_redraw = False
+        do_change_scrollbar = False
         
         if event == "back":
             self.parent_server.remove_client(self)
@@ -352,14 +438,18 @@ class ProtoMenu(Client):
                 self.selected -= 1
                 do_redraw = True
                 max_scroll = self.selected * self.item_height
-                if self.scroll > max_scroll: self.scroll = max_scroll
+                if self.scroll > max_scroll:
+                    self.scroll = max_scroll
+                    do_change_scrollbar = True
                 
         if event == "down":
             if self.selected < self.item_count - 1:
                 self.selected += 1
                 do_redraw = True
                 min_scroll = (self.selected + 1) * self.item_height - self.view_height
-                if self.scroll < min_scroll: self.scroll = min_scroll
+                if self.scroll < min_scroll:
+                    self.scroll = min_scroll
+                    do_change_scrollbar = True
         
         if event == "quit" or event == "offscreen":
             self.quit_flag = True
@@ -368,6 +458,11 @@ class ProtoMenu(Client):
         if event == "ok":
             func = self.items[self.selected][1]
             if func != None: func(self.parent_server)
+        
+        if do_change_scrollbar:
+            scrollbar_max = self.view_height - self.scrollbar_height + 1
+            scroll_max = self.content_height - self.view_height
+            self.scrollbar_pos = scrollbar_max * self.scroll / scroll_max
         
         if do_redraw: self.dirty.set()
 
--- menu.py
+++ menu.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 import pygame
+import pygame.freetype
 from bling import *
 import os
 import time
@@ -26,7 +27,10 @@ class MainMenu(ProtoMenu):
         def spawn_settings_menu(server): server.add_client(PlaceholderMenu(graf_props))
         items.append(("Settings>", spawn_settings_menu))
         
-        ProtoMenu.__init__(self, graf_props = graf_props, items = items)
+        ProtoMenu.__init__(self, graf_props = graf_props, items = items, title = "Tick")
+    
+    def event(self, event):
+        if event != "back": ProtoMenu.event(self, event)
 
 class PlaceholderMenu(ProtoMenu):
     def __init__(self, graf_props):
@@ -37,7 +41,7 @@ class PlaceholderMenu(ProtoMenu):
             ("  menu.     :-(", None)
         ]
         
-        ProtoMenu.__init__(self, graf_props = graf_props, items = items)
+        ProtoMenu.__init__(self, graf_props = graf_props, items = items, title = "Dang it!")
 
 #closures in Python are an absolute fucking joke!
 
@@ -58,7 +62,7 @@ class ArtistsMenu(ProtoMenu):
             if artist == None: artist = "*See all albums"
             items.append((artist + ">", spawn_artist_albums_menu))
         
-        ProtoMenu.__init__(self, graf_props = graf_props, items = items)
+        ProtoMenu.__init__(self, graf_props = graf_props, items = items, title = "Artists")
 
 class ArtistAlbumsMenu(ProtoMenu):
     def __init__(self, artist, graf_props):
@@ -80,66 +84,96 @@ class ArtistAlbumsMenu(ProtoMenu):
             menuspawner = create_menuspawner_for_album(artist, album)
             items.append((album + ">", menuspawner))
         
-        ProtoMenu.__init__(self, graf_props = graf_props, items = items)
+        ProtoMenu.__init__(self, graf_props = graf_props, items = items, title = artist)
 
 class AlbumSongsMenu(ProtoMenu):
     def __init__(self, artist, album, graf_props):
         songs = mpd_client.find("ARTIST", artist, "ALBUM", album)
         items = []
         
-        def create_menuspawner_for_song(file): # aka artist_albums_menuspawner_factory
+        def create_action_for_song(file): # aka artist_albums_menuspawner_factory
             def menuspawner(server): # aka artist_albums_menu_factory
-                server.add_client(SongSelectedMenu(graf_props=graf_props, file=file))
+                mpd_status = mpd_client.status()
+                if int(mpd_status["playlistlength"]) > 1:
+                    # menu: now, next, add to queue (Nth)
+                    server.add_client(SongSelectedMenu(graf_props=graf_props, file=file, canqueue=True))
+                elif mpd_status["state"] != "stop":
+                    # menu: now, next
+                    server.add_client(SongSelectedMenu(graf_props=graf_props, file=file, canqueue=True))
+                else:
+                    # just play it
+                    id = mpd_client.addid(file)
+                    mpd_client.playid(id)
+                
             return menuspawner
         
         for song in songs:
-            menuspawner = create_menuspawner_for_song(song["file"])
+            menuspawner = create_action_for_song(song["file"])
             title = song["title"]
             items.append((title, menuspawner))
         
-        ProtoMenu.__init__(self, graf_props = graf_props, items = items)
+        ProtoMenu.__init__(self, graf_props = graf_props, items = items, title = album)
         
 class SongSelectedMenu(ProtoMenu):
-    def __init__(self, file, graf_props):
+    def __init__(self, file, canqueue, graf_props):
         items = []
+                
+        # def play_next(server):
+        #     id = mpd_client.addid(file)
+        #     print mpd_client.currentsong()
+        #     server.remove_client(self)
+        # items.append(("Play next", play_next))
         
-        def just_play_the_song(server):
+        if canqueue:
+            mpd_status = mpd_client.status()
+            
+            queue_pos = int(mpd_status["playlistlength"]) + 1
+            if mpd_status["playlistlength"] != "stop": queue_pos -= 1
+            
+            def queue_song(server):
+                mpd_client.add(file)
+                mpd_client.play()
+                server.remove_client(self)
+            items.append(("Add to queue (#%d)" % queue_pos, queue_song))
+        
+        def play_now(server):
             mpd_client.clear()
             mpd_client.add(file)
             mpd_client.play(0)
             server.remove_client(self)
+        items.append(("Play now", play_now))
         
-        items.append(("Play it!", just_play_the_song))
-        
-        ProtoMenu.__init__(self, graf_props = graf_props, items = items)
+        ProtoMenu.__init__(self, graf_props = graf_props, items = items, title = "Play song…")
 
 
 
 
-print "Initialising PyGame... video output should be pink"
+print("Initialising PyGame... video output should be pink")
 os.putenv('SDL_VIDEODRIVER', 'fbcon') # What the fuck?
 pygame.display.init()
+print("PyGame version " + pygame.version.ver)
 disp_info = pygame.display.Info()
 screen = pygame.display.set_mode((disp_info.current_w, disp_info.current_h), pygame.FULLSCREEN)
 screen.fill((127, 0, 127))        
 pygame.display.update()
 pygame.font.init()
+pygame.freetype.init()
 
-print "Chooking up mpd"
+print("Chooking up mpd")
 global mpd_client
 mpd_client = mpd.MPDClient()
 mpd_client.timeout = 10
 mpd_client.idletimeout = None
 mpd_client.connect("localhost", 6600)
 
-print "Putting on Bling!"
+print("Putting on Bling!")
 palette = tuple([(i, i, i) for i in range(0, 255)])
 
 graf_props = (128, 64, 8, palette)
 
 driver = ST7575Server()
 input_server = StdinServer()
-compositor = Compositor(graf_props)
+compositor = FabCompositor(graf_props)
 input_server.add_client(compositor)
 driver.add_client(compositor)
 
