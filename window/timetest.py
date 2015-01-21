4081257d998dd160bdb7d43cef0aafa03acd2783
Elliot Nunn <elliotnunn@me.com>
2015-01-21 07:57:25 +0000

    fixed license, again: GPLv3+

--- window/timetest.py
+++ window/timetest.py
@@ -1,3 +1,18 @@
+# This file is part of bling.
+
+# Bling is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Bling is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Bling.  If not, see <http://www.gnu.org/licenses/>.
+
 class TimeTest(bling_core.Client):
     def _setup(self, graf_props):
         self.font = pygame.freetype.Font("chicago.bdf")
